5449873813c74e8e7ca7aaf6319211ba93a6ea13
Jaime Rodriguez Dennis <jairoden181203@gmail.com>
2024-05-25 23:37:48 -0700

    Practica 5: Cotizacion

---
+++ app/src/main/java/com/example/appholamundo/cotizacionmainActivity.java
@@ -0,0 +1,55 @@
+package com.example.appholamundo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class cotizacionmainActivity extends AppCompatActivity {
+
+    private EditText txtPregunta;
+    private Button btnCotizacion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cotizacionmain);
+        iniciarcomponentes();
+
+       btnCotizacion.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               // Obtener el texto del EditText
+               String pregunta = txtPregunta.getText().toString();
+
+               // Crear un Intent para iniciar la actividad de cotización
+               Intent intent = new Intent(cotizacionmainActivity.this, cotizacionActivity.class);
+
+               // Agregar el texto como un extra al Intent
+               intent.putExtra("PREGUNTA", pregunta);
+
+               // Iniciar la actividad de cotización
+               startActivity(intent);
+           }
+       });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void iniciarcomponentes(){
+        txtPregunta = (EditText) findViewById(R.id.txtPregunta);
+        btnCotizacion = (Button) findViewById(R.id.btnIrCotizacion);
+    }
+}
